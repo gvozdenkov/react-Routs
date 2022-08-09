436889895bae9888c5e48ee73e2c8556456abb9e
arty gvozdenkov <gvozdenkov@gmail.com>
2022-08-09 15:21:57 +0300

    refact: move nav to NavBar component. Move header style to /block

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,12 @@
 import React from "react";
 import "../components/Header.css";
+import NavBar from "./NavBar";
 
 function Header() {
   return (
     <header className="header page__section">
       <h2 className="header__title">🤔 Emoji Critic</h2>
-      <ul className="nav">
-        <li>
-          <a href="/" className="nav__item">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/reviews" className="nav__item">
-            Emoji Review
-          </a>
-        </li>
-        <li>
-          <a href="/about-me" className="nav__item">
-            About Me
-          </a>
-        </li>
-        <li>
-          <a href="/about-us" className="nav__item">
-            About Us
-          </a>
-        </li>
-      </ul>
+      <NavBar />
     </header>
   );
 }
